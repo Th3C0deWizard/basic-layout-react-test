b69cfedfc58dd1c7369d6e7d724f44c975dfd181
Ttrejosg <tomtrejosgil@gmail.com>
2024-08-20 19:03:55 -0500

    Game cards added in main__products and footer added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,27 @@
-import '../styles/Footer.css'
+import "../styles/Footer.css";
 
 export default function Footer() {
   return (
     <footer className="footer">
-      This is the footer
+      <img src="/src/assets/aliPlay__logo.jpeg" alt="aliPlay Logo" />
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#menu">Menu</a>
+        </li>
+        <li>
+          <a href="order">Order</a>
+        </li>
+        <li>
+          <a href="blog">Blog</a>
+        </li>
+        <li>
+          <a href="contact">Contact</a>
+        </li>
+      </ul>
+      <p>Copyright 2024 by AliPlay All rights reserved</p>
     </footer>
-  )
+  );
 }
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,15 +1,72 @@
-import '../styles/Main.css'
+import "../styles/Main.css";
+import GameCard from "./GameCard";
+
+const games = [
+  {
+    title: "Minecraft",
+    price: 60,
+    image: "https://via.placeholder.com/150",
+    category: "action-adventure",
+  },
+  {
+    title: "Just Dance 2021",
+    price: 45,
+    image: "https://via.placeholder.com/150",
+    category: "platformer",
+  },
+  {
+    title: "The last of us",
+    price: 58,
+    image: "https://via.placeholder.com/150",
+    category: "racing",
+  },
+  {
+    title: "Splatoon 2",
+    price: 63,
+    image: "https://via.placeholder.com/150",
+    category: "shooter",
+  },
+  {
+    title: "Super Smash Bros. Ultimate",
+    price: 39,
+    image: "https://via.placeholder.com/150",
+    category: "fighting",
+  },
+  {
+    title: "Animal Crossing: New Horizons",
+    price: 40,
+    image: "https://via.placeholder.com/150",
+    category: "simulation",
+  },
+  {
+    title: "Pokémon Sword and Shield",
+    price: 50,
+    image: "https://via.placeholder.com/150",
+    category: "role-playing",
+  },
+  {
+    title: "Red Dead Redemption 2",
+    price: 35,
+    image: "https://via.placeholder.com/150",
+    category: "action-adventure",
+  },
+];
 
 export default function Main() {
   return (
     <main className="main">
-      <aside className="main__aside">
-        Contenido lateral
-      </aside>
+      <aside className="main__aside">Contenido lateral</aside>
       <section className="main__products">
-        Contenido principal
+        {games.map((game, index) => (
+          <GameCard
+            key={index}
+            title={game.title}
+            price={game.price}
+            image={game.image}
+            category={game.category}
+          />
+        ))}
       </section>
     </main>
-  )
+  );
 }
-
