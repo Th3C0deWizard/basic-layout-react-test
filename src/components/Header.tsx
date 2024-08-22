734acd1c7fe981be8c524454674eb7dad7e92e42
Carlos Paez <carlosa.paezc@autonoma.edu.co>
2024-08-22 16:13:37 -0500

    initial header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,39 @@
 import '../styles/Header.css'
+import UserProfileIcon from '../icons/UserProfileIcon';
+import type { ReactNode } from 'react';
+
+type NavLink = {
+  label: ReactNode
+  href: string
+}
 
 export default function Header() {
+
+  const navLinks: Array<NavLink> = [
+    { label: "Games", href: "#games" },
+    { label: "Friends", href: "#friends" },
+    { label: <UserProfileIcon fill="#fff" width="40px" height="40px" />, href: "#myprofile" },
+  ];
+
   return (
     <header className="header">
-      header for course
+      <div className='logo'>
+        <a href="/">AliPlay!</a>
+      </div>
+      <nav className='navbar'>
+        <ul>
+          {navLinks.map(({ label, href }) => (
+            <li className='nav-link'>
+              <a href={href}>{label}</a>
+            </li>
+          ))}
+        </ul>
+        <ul>
+          <li>
+
+          </li>
+        </ul>
+      </nav>
     </header>
   )
 }
