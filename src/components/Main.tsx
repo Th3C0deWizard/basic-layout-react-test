241106d0eabe060ff215b396e76d21df6ab50f83
Ttrejosg <tomtrejosgil@gmail.com>
2024-08-22 17:21:09 -0500

    Products container component for GameCards added

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,15 +1,9 @@
-import '../styles/Main.css'
+import "../styles/Main.css";
 
 export default function Main() {
   return (
     <main className="main">
-      <aside className="main__aside">
-        Contenido lateral
-      </aside>
-      <section className="main__products">
-        Contenido principal
-      </section>
+      <aside className="main__aside">Contenido lateral</aside>
     </main>
-  )
+  );
 }
-
